25fe47011128ce33bc1a59554717e88d9d847a3b
Brandon Catubig <firezip@gmail.com>
2012-04-08 01:11:10 -0400

    added main

---
+++ CheckersGUI.java
@@ -0,0 +1,29 @@
+/**
+ *  __  __                                        __      _____
+   / / / /___  ____ ___  ___ _      ______  _____/ /__   / ___/
+  / /_/ / __ \/ __ `__ \/ _ \ | /| / / __ \/ ___/ //_/  / __ \ 
+ / __  / /_/ / / / / / /  __/ |/ |/ / /_/ / /  / ,<    / /_/ / 
+/_/ /_/\____/_/ /_/ /_/\___/|__/|__/\____/_/  /_/|_|   \____/  
+======================="Awww Sheeeit"=========================
+
+Brandon Catubig
+4/8/2012
+COP 3330
+A simple GUI checkers game
+ */
+
+/**
+ * @author Brandon Catubig
+ *
+ */
+public class CheckersGUI {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
